17da53b7d32d88bcff05b351389cd040b6786e21
Oris Feliz <of33@njit.edu>
2018-10-04 17:48:29 -0400

    began setting up health and UI, no actual code written

---
+++ Metroidvania/Assets/Scripts/UI/Health_UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_UI : MonoBehaviour 
+{
+    public GameObject playerHealth;
+    public Text displayPHealth;
+    int p_health;
+
+	// Use this for initialization
+	void Start () 
+    {
+		//int p_health = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
